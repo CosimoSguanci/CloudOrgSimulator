ea1d9c66468d5f23e171dcd095648dae1cfd0aac
CosimoSguanci <cosimo.sguanci@mail.polimi.it>
2021-09-18 16:06:13 -0500

    updated configuration, configurable Vm Scaling

--- src/main/scala/Simulations/SimulationSuiteServices.scala
+++ src/main/scala/Simulations/SimulationSuiteServices.scala
@@ -2,16 +2,17 @@ package Simulations
 
 import HelperUtils.{CreateLogger, ObtainConfigReference}
 import Simulations.BasicFirstExampleSimulation.config
-import org.cloudbus.cloudsim.allocationpolicies.VmAllocationPolicySimple
+import org.cloudbus.cloudsim.allocationpolicies.{VmAllocationPolicyRandom, VmAllocationPolicySimple}
 import org.cloudbus.cloudsim.brokers.DatacenterBrokerSimple
 import org.cloudbus.cloudsim.cloudlets.{Cloudlet, CloudletSimple}
 import org.cloudbus.cloudsim.core.CloudSim
 import org.cloudbus.cloudsim.datacenters.network.NetworkDatacenter
 import org.cloudbus.cloudsim.datacenters.{Datacenter, DatacenterSimple}
+import org.cloudbus.cloudsim.distributions.{ContinuousDistribution, UniformDistr}
 import org.cloudbus.cloudsim.hosts.{Host, HostSimple}
-import org.cloudbus.cloudsim.resources.{DatacenterStorage, File, HarddriveStorage, Pe, PeSimple, SanStorage}
+import org.cloudbus.cloudsim.resources.*
 import org.cloudbus.cloudsim.utilizationmodels.UtilizationModelDynamic
-import org.cloudbus.cloudsim.vms.{Vm, VmSimple}
+import org.cloudbus.cloudsim.vms.{Vm, VmCost, VmSimple}
 import org.cloudsimplus.autoscaling.VmScaling
 import org.cloudsimplus.builders.tables.CloudletsTableBuilder
 import utils.{ScalingStrategy, VmWithScalingFactory}
@@ -31,7 +32,7 @@ object SimulationSuiteServices:
   def Start() = {
     val simulation = new CloudSim()
     //simulation.terminateAt(config.getDouble("simulationSuiteServices.duration"))
-    val numOfDatacenters = config.getInt("simulationSuiteServices.numOfDatacenters")
+    val numOfDatacenters = config.getInt("simulationSuiteServices.datacenter.num")
 
     val datacenters: List[Datacenter] = createDatacenters(simulation, List.empty, numOfDatacenters)
     val datacenterStorageList: List[DatacenterStorage] = createDatacenterStorage(numOfDatacenters);
@@ -56,6 +57,8 @@ object SimulationSuiteServices:
 
     val finishedCloudlets = broker0.getCloudletFinishedList()
     new CloudletsTableBuilder(finishedCloudlets).build()
+
+    printCost(i = 0, broker0, totalCost = 0, memoryTotalCost = 0, processingTotalCost = 0, storageTotalCost = 0, bwTotalCost = 0, totalNonIdleVms = 0)
   }
 
   def createDatacenters(simulation: CloudSim, datacenters: List[NetworkDatacenter], numOfDatacenters: Int): List[NetworkDatacenter] = {
@@ -74,7 +77,20 @@ object SimulationSuiteServices:
     val range = 1 to numOfHosts
     val hostList: List[Host] = range.map(i => createHost()).toList
 
-    new NetworkDatacenter(simulation, hostList.asJava, new VmAllocationPolicySimple());
+    //.setSchedulingInterval();
+
+
+    val random: ContinuousDistribution = new UniformDistr();
+    val datacenter: NetworkDatacenter = new NetworkDatacenter(simulation, hostList.asJava, new VmAllocationPolicyRandom(random));
+
+    datacenter
+      .getCharacteristics()
+      .setCostPerSecond(config.getDouble("simulationSuiteServices.datacenter.costPerSecond"))
+      .setCostPerMem(config.getDouble("simulationSuiteServices.datacenter.costPerMem"))
+      .setCostPerStorage(config.getDouble("simulationSuiteServices.datacenter.costPerStorage"))
+      .setCostPerBw(config.getDouble("simulationSuiteServices.datacenter.costPerBw"));
+
+    return datacenter
   }
 
   def createHost(): Host = {
@@ -89,31 +105,31 @@ object SimulationSuiteServices:
   }
 
   def createDatacenterStorage(numOfDatacenters: Int): List[DatacenterStorage] = {
-    val numOfSanForDatacenter = config.getInt("simulationSuiteServices.numOfSanForDatacenter")
-    val sanStorageCapacity = config.getLong("simulationSuiteServices.sanStorageCapacityForDatacenter")
-    val sanBW = config.getDouble("simulationSuiteServices.sanBWInMBps")
-    val sanNetworkLatency = config.getDouble("simulationSuiteServices.sanNetworkLatencySec")
+    val numOfSanForDatacenter = config.getInt("simulationSuiteServices.datacenter.numOfSanForDatacenter")
+    val sanStorageCapacity = config.getLong("simulationSuiteServices.datacenter.sanStorageCapacityForDatacenter")
+    val sanBW = config.getDouble("simulationSuiteServices.datacenter.sanBWInMBps")
+    val sanNetworkLatency = config.getDouble("simulationSuiteServices.datacenter.sanNetworkLatencySec")
 
     val range = 1 to numOfDatacenters
 
     range.map(i => {
       val sanRange = 1 to numOfSanForDatacenter
-      val sanList: List[SanStorage] = sanRange.map(j => new SanStorage(sanStorageCapacity, sanBW, sanNetworkLatency )).toList
+      val sanList: List[SanStorage] = sanRange.map(j => new SanStorage(sanStorageCapacity, sanBW, sanNetworkLatency)).toList
       sanList.foreach(san => addFilesToSan(san));
       new DatacenterStorage(sanList.asJava)
     }).toList
   }
 
   def addFilesToSan(san: SanStorage): Unit = {
-    val numOfStoredFiles = config.getInt("simulationSuiteServices.numOfStoredFiles")
-    val sizeSmall = config.getInt("simulationSuiteServices.sanFileSizeInMB_small")
-    val sizeMedium = config.getInt("simulationSuiteServices.sanFileSizeInMB_medium")
-    val sizeBig = config.getInt("simulationSuiteServices.sanFileSizeInMB_big")
+    val numOfStoredFiles = config.getInt("simulationSuiteServices.datacenter.numOfStoredFiles")
+    val sizeSmall = config.getInt("simulationSuiteServices.datacenter.sanFileSizeInMB_small")
+    val sizeMedium = config.getInt("simulationSuiteServices.datacenter.sanFileSizeInMB_medium")
+    val sizeBig = config.getInt("simulationSuiteServices.datacenter.sanFileSizeInMB_big")
 
     val range = 1 to numOfStoredFiles
 
     range.foreach(i => {
-      val size = if(i < (numOfStoredFiles / 3)) then sizeSmall else if(i < 2 * (numOfStoredFiles / 3)) then sizeMedium else sizeBig
+      val size = if (i < (numOfStoredFiles / 3)) then sizeSmall else if (i < 2 * (numOfStoredFiles / 3)) then sizeMedium else sizeBig
       val file = new File(s"file$i.txt", size);
       san.addFile(file);
     })
@@ -121,15 +137,13 @@ object SimulationSuiteServices:
 
   def createVms(): List[Vm] = {
     val scalingStrategyId = config.getInt("simulationSuiteServices.vm.scalingStrategy");
-    val numOfVMs = config.getInt("simulationSuiteServices.vm.size")
-    val hostMips = config.getInt("simulationSuiteServices.host.mipsCapacity")
-    val vmPEs = config.getInt("simulationSuiteServices.vm.PEs")
+    val numOfVMs = config.getInt("simulationSuiteServices.vm.num")
     val range = 1 to numOfVMs
-    range.map(i => VmWithScalingFactory(hostMips, vmPEs, scalingStrategyId)).toList
+    range.map(i => VmWithScalingFactory(scalingStrategyId)).toList
   }
 
   def createCloudlets(): List[Cloudlet] = {
-    val numOfCloudlets = config.getInt("simulationSuiteServices.cloudlet.size")
+    val numOfCloudlets = config.getInt("simulationSuiteServices.cloudlet.num")
     val utilizationModel: UtilizationModelDynamic = new UtilizationModelDynamic(config.getDouble("simulationSuiteServices.utilizationRatio"))
     val cloudletLength = config.getInt("simulationSuiteServices.cloudlet.length")
     val cloudletPEs = config.getInt("simulationSuiteServices.cloudlet.PEs")
@@ -145,5 +159,42 @@ object SimulationSuiteServices:
         cloudlet.addRequiredFile(s"file$i.txt");
       }
     }
+  }
+
+  def printCost(
+                 i: Int,
+                 broker: DatacenterBrokerSimple,
+                 totalCost: Double,
+                 processingTotalCost: Double,
+                 memoryTotalCost: Double,
+                 storageTotalCost: Double,
+                 bwTotalCost: Double,
+                 totalNonIdleVms: Int): Unit = {
+
+    if(i == broker.getVmCreatedList().size()) {
+      // finished iterating over vms, we print the final report
+      println("Total cost ($) for " + totalNonIdleVms +
+        " created VMs from " + broker.getVmsNumber() + ": " +
+        processingTotalCost + "$ " +
+        memoryTotalCost + "$ " +
+        storageTotalCost + "$ " +
+        bwTotalCost + "$ " +
+        totalCost + "$")
+
+      return;
+    }
+
+    val vm: Vm = broker.getVmCreatedList().get(i);
+    val vmCost: VmCost = new VmCost(vm)
+    println(vmCost)
+
+    val newTotalCost: Double = totalCost + vmCost.getTotalCost()
+    val newProcessingTotalCost: Double = processingTotalCost + vmCost.getProcessingCost()
+    val newMemoryTotalCost: Double = memoryTotalCost + vmCost.getMemoryCost()
+    val newStorageTotalCost: Double = storageTotalCost + vmCost.getStorageCost()
+    val newBwTotalCost: Double = bwTotalCost + vmCost.getBwCost()
+    val newTotalNonIdleVms: Int = if(vm.getTotalExecutionTime() > 0) then totalNonIdleVms + 1 else totalNonIdleVms
+
+    printCost(i+1, broker, newTotalCost, newProcessingTotalCost, newMemoryTotalCost, newStorageTotalCost, newBwTotalCost, newTotalNonIdleVms)
   }
 
--- src/main/scala/utils/VmWithScalingFactory.scala
+++ src/main/scala/utils/VmWithScalingFactory.scala
@@ -2,38 +2,166 @@ package utils
 
 import HelperUtils.{CreateLogger, ObtainConfigReference}
 import Simulations.BasicCloudSimPlusExample
-import org.cloudbus.cloudsim.resources.Processor
+import Simulations.SimulationSuiteServices.config
+import org.cloudbus.cloudsim.resources.{Bandwidth, Processor, Ram}
 import org.cloudbus.cloudsim.vms.{Vm, VmSimple}
+import org.cloudsimplus.autoscaling.resources.{ResourceScalingGradual, ResourceScalingInstantaneous}
 import org.cloudsimplus.autoscaling.{HorizontalVmScalingSimple, VerticalVmScalingSimple, VmScaling}
 import org.cloudsimplus.listeners.VmHostEventInfo
 
+import java.util.function
+import java.util.function.{Predicate, Supplier}
+
 enum ScalingStrategy:
-  case HORIZONTAL, VERTICAL, NONE
+  case HORIZONTAL, VERTICAL, BOTH, NONE
 
 object VmWithScalingFactory {
-  def apply(hostMips: Int, VmPEs: Int, scalingStrategyId: Int): Vm = { // vm storage?
+  def apply(scalingStrategyId: Int): Vm = { // vm storage?
     
-    val config = ObtainConfigReference("simulationSuiteServices") match {
+/*    val config = ObtainConfigReference("simulationSuiteServices") match {
       case Some(value) => value
       case None => throw new RuntimeException("Cannot obtain a reference to the config data.")
     }
-    val logger = CreateLogger(classOf[BasicCloudSimPlusExample])
+    val logger = CreateLogger(classOf[BasicCloudSimPlusExample])*/
     
-    val vm = new VmSimple(hostMips, VmPEs);
+    val vm = createVm()
 
     val scalingStrategy: ScalingStrategy = mapScalingIdToStrategy(scalingStrategyId);
 
     scalingStrategy match {
-      case ScalingStrategy.HORIZONTAL => vm.setHorizontalScaling(new HorizontalVmScalingSimple())
-      case ScalingStrategy.VERTICAL => vm.setPeVerticalScaling(new VerticalVmScalingSimple(classOf[Processor], config.getDouble("simulationSuiteServices.scalingFactor")))// example, to be tuned
+      case ScalingStrategy.HORIZONTAL => buildHorizontalScaling(vm)
+      case ScalingStrategy.VERTICAL => buildVerticalScaling(vm)
+      case ScalingStrategy.BOTH => {
+        buildHorizontalScaling(vm)
+        buildVerticalScaling(vm)
+      }
       case _ => vm
     }
+  }
+
+  def createVm(): Vm = {
+    val hostMips = config.getInt("simulationSuiteServices.host.mipsCapacity")
+    val vmPEs = config.getInt("simulationSuiteServices.vm.PEs")
+    val ramInMBs = config.getInt("simulationSuiteServices.vm.RAMInMBs")
+    val storageInMBs = config.getLong("simulationSuiteServices.vm.StorageInMBs")
+    val bwInMBps = config.getInt("simulationSuiteServices.vm.BandwidthInMBps")
+
+    new VmSimple(hostMips, vmPEs)
+      .setRam(ramInMBs)
+      .setBw(bwInMBps)
+      .setSize(storageInMBs)
+  }
+
+  def isVmOverloaded(vm: Vm): Boolean = {
+    val cpuOverloadingThreshold = config.getDouble("simulationSuiteServices.vm.autoscaling.horizontalScaling.cpuOverloadedThreshold")
+    vm.getCpuPercentUtilization() > cpuOverloadingThreshold
+  }
+
+  def buildHorizontalScaling(vm: Vm): Vm = {
+    val horizontalScaling: HorizontalVmScalingSimple = new HorizontalVmScalingSimple();
+    horizontalScaling
+      .setVmSupplier(new Supplier[Vm] {
+        override def get(): Vm = createVm()
+      })
+      .setOverloadPredicate(new Predicate[Vm] {
+        override def test(t: Vm): Boolean = isVmOverloaded(t)
+      })
+    vm.setHorizontalScaling(horizontalScaling)
+  }
+
+  def buildVerticalScaling(vm: Vm): Vm = {
+    val cpuVerticalScalingEnabled: Boolean = config.getInt("simulationSuiteServices.vm.autoscaling.cpuVerticalScaling.enabled") == 1
+    val ramVerticalScalingEnabled: Boolean = config.getInt("simulationSuiteServices.vm.autoscaling.ramVerticalScaling.enabled") == 1
+    val bwVerticalScalingEnabled: Boolean = config.getInt("simulationSuiteServices.vm.autoscaling.bwVerticalScaling.enabled") == 1
+
+    if(cpuVerticalScalingEnabled) {
+      val cpuVerticalScaling: VerticalVmScalingSimple = new VerticalVmScalingSimple(classOf[Processor], config.getDouble("simulationSuiteServices.vm.autoscaling.cpuVerticalScaling.scalingFactor"))
+      cpuVerticalScaling.setResourceScaling(new ResourceScalingGradual()) // Here we are not using an instantaneous resource scaling technique since we can tolerate some loss in SLA to avoid unneeded VM scaling
+
+      val lowerThresholdFunction: java.util.function.Function[Vm, java.lang.Double] = new java.util.function.Function[Vm, java.lang.Double] {
+        override def apply(vm: Vm): java.lang.Double = cpuLowerUtilizationThreshold()
+      }
+
+      val upperThresholdFunction: java.util.function.Function[Vm, java.lang.Double] = new java.util.function.Function[Vm, java.lang.Double] {
+        override def apply(vm: Vm): java.lang.Double = cpuUpperUtilizationThreshold()
+      }
+
+      cpuVerticalScaling.setLowerThresholdFunction(lowerThresholdFunction)
+      cpuVerticalScaling.setUpperThresholdFunction(upperThresholdFunction)
+
+      vm.setPeVerticalScaling(cpuVerticalScaling);
+    }
+
+    if(ramVerticalScalingEnabled) {
+      val ramVerticalScaling: VerticalVmScalingSimple = new VerticalVmScalingSimple(classOf[Ram], config.getDouble("simulationSuiteServices.vm.autoscaling.ramVerticalScaling.scalingFactor"))
+      ramVerticalScaling.setResourceScaling(new ResourceScalingGradual()) // Here we are not using an instantaneous resource scaling technique since we can tolerate some loss in SLA to avoid unneeded VM scaling
+
+      val lowerThresholdFunction: java.util.function.Function[Vm, java.lang.Double] = new java.util.function.Function[Vm, java.lang.Double] {
+        override def apply(vm: Vm): java.lang.Double = ramLowerUtilizationThreshold()
+      }
+
+      val upperThresholdFunction: java.util.function.Function[Vm, java.lang.Double] = new java.util.function.Function[Vm, java.lang.Double] {
+        override def apply(vm: Vm): java.lang.Double = ramUpperUtilizationThreshold()
+      }
+
+      ramVerticalScaling.setLowerThresholdFunction(lowerThresholdFunction)
+      ramVerticalScaling.setUpperThresholdFunction(upperThresholdFunction)
+
+      vm.setPeVerticalScaling(ramVerticalScaling);
+    }
+
+    if(bwVerticalScalingEnabled) {
+      val bwVerticalScaling: VerticalVmScalingSimple = new VerticalVmScalingSimple(classOf[Bandwidth], config.getDouble("simulationSuiteServices.vm.autoscaling.ramVerticalScaling.scalingFactor"))
+      bwVerticalScaling.setResourceScaling(new ResourceScalingGradual()) // Here we are not using an instantaneous resource scaling technique since we can tolerate some loss in SLA to avoid unneeded VM scaling
+
+      val lowerThresholdFunction: java.util.function.Function[Vm, java.lang.Double] = new java.util.function.Function[Vm, java.lang.Double] {
+        override def apply(vm: Vm): java.lang.Double = bwLowerUtilizationThreshold()
+      }
+
+      val upperThresholdFunction: java.util.function.Function[Vm, java.lang.Double] = new java.util.function.Function[Vm, java.lang.Double] {
+        override def apply(vm: Vm): java.lang.Double = bwUpperUtilizationThreshold()
+      }
+
+      bwVerticalScaling.setLowerThresholdFunction(lowerThresholdFunction)
+      bwVerticalScaling.setUpperThresholdFunction(upperThresholdFunction)
+
+      vm.setPeVerticalScaling(bwVerticalScaling);
+    }
+
+    return vm
+
+  }
+
+  // threshold to determine if the vm is underloaded
+  def cpuLowerUtilizationThreshold(): Double = {
+    return config.getDouble("simulationSuiteServices.vm.autoscaling.cpuVerticalScaling.lowerUtilizationThreshold")
+  }
+
+  def cpuUpperUtilizationThreshold(): Double = {
+    return config.getDouble("simulationSuiteServices.vm.autoscaling.cpuVerticalScaling.upperUtilizationThreshold")
+  }
+
+  def ramLowerUtilizationThreshold(): Double = {
+    return config.getDouble("simulationSuiteServices.vm.autoscaling.ramVerticalScaling.lowerUtilizationThreshold")
+  }
+
+  def ramUpperUtilizationThreshold(): Double = {
+    return config.getDouble("simulationSuiteServices.vm.autoscaling.ramVerticalScaling.upperUtilizationThreshold")
+  }
+
+  def bwLowerUtilizationThreshold(): Double = {
+    return config.getDouble("simulationSuiteServices.vm.autoscaling.bwVerticalScaling.lowerUtilizationThreshold")
+  }
+
+  def bwUpperUtilizationThreshold(): Double = {
+    return config.getDouble("simulationSuiteServices.vm.autoscaling.bwVerticalScaling.upperUtilizationThreshold")
   }
 
   def mapScalingIdToStrategy(id: Int): ScalingStrategy = {
     id match {
       case 0 => ScalingStrategy.HORIZONTAL
       case 1 => ScalingStrategy.VERTICAL
+      case 2 => ScalingStrategy.BOTH
       case _ => ScalingStrategy.NONE
     }
   }
